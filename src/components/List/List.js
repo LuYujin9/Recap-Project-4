6beffcb73b85756c4bec2fa1bca4d37b563600d5
YujinLu9 <wuwujane@hotmail.com>
2023-03-09 17:48:40 +0100

    deconstructed weather

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,21 +1,22 @@
 import "./List.css";
 export default function List({ activities, weather, onDeleteActivity }) {
-  console.log(weather);
+  console.log("the weather", weather);
   console.log(activities);
+  const { condition, temperature, isGoodWeather } = weather;
   return (
     <>
       <h2>
-        <span alt="emoji showing weathercondition">{weather.condition}</span>{" "}
-        {weather.temperature}°
+        <span alt="emoji showing weathercondition">{condition}</span>{" "}
+        {temperature}
       </h2>
       <p>
-        {weather.isGoodWeather
+        {isGoodWeather
           ? "The weather is awesone/ Go outside and:"
           : "Bad weather outside/ Here's what you can do now:"}
       </p>
       <ul>
         {activities.map((activity) => (
-          <li className="list-item" id={activity.id}>
+          <li className="list-item" key={activity.id} id={activity.id}>
             {activity.name}
             <button
               aria-label="delete button"
